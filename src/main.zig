02ab586c1d95c482e4b45206e025726c5ebc142c
mark.ahn <ahn8202@gmail.com>
2023-09-05 21:19:17 +0900

    fixes logic

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,43 @@
 const std = @import("std");
 const testing = std.testing;
 
-const gpa = std.heap.GeneralPurposeAllocator(.{}){};
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-pub fn init() !void {}
-pub fn deinit() void {
-    defer if (gpa.deinit() == .leak) std.debug.panic("memory leak @ zigs.deinit()", .{});
+pub const InitState = enum {
+    uninited,
+    inited,
+    deinited,
+};
+
+pub const Error = error{
+    double_init,
+    deinit_before_init,
+    gpa_leak,
+};
+
+var init_st: InitState = .uninited;
+pub fn init() !void {
+    var st = @atomicRmw(InitState, &init_st, std.builtin.AtomicRmwOp.Xchg, .inited, std.builtin.AtomicOrder.Monotonic);
+    switch (st) {
+        .inited => return,
+        .deinited => return Error.double_init,
+        .uninited => {},
+    }
+}
+pub fn deinit() !void {
+    var st = @atomicRmw(InitState, &init_st, std.builtin.AtomicRmwOp.Xchg, .deinited, std.builtin.AtomicOrder.Monotonic);
+    switch (st) {
+        .inited => {},
+        .deinited => return,
+        .uninited => return Error.deinit_before_init,
+    }
+
+    if (gpa.deinit() == .leak) return Error.gpa_leak;
+
+    return;
+}
+
+test "module" {
+    try init();
+    defer deinit() catch |err| std.debug.panic("error during deinit - {s}\n", .{@errorName(err)});
 }

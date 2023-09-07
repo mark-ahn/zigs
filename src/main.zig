16330f492617f09ca57ce614e29608a254339c0b
mark.ahn <ahn8202@gmail.com>
2023-09-07 09:55:43 +0900

    refactors namespace

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-pub var ally = gpa.allocator();
+pub const heap = struct {
+    pub var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    pub var ally = gpa.allocator();
+};
 
 pub const InitState = enum {
     uninited,
@@ -16,9 +18,11 @@ pub const Error = error{
     gpa_leak,
 };
 
-var init_st: InitState = .uninited;
+const global = struct {
+    var init_st: InitState = .uninited;
+};
 pub fn init() !void {
-    var st = @atomicRmw(InitState, &init_st, std.builtin.AtomicRmwOp.Xchg, .inited, std.builtin.AtomicOrder.Monotonic);
+    var st = @atomicRmw(InitState, &global.init_st, std.builtin.AtomicRmwOp.Xchg, .inited, std.builtin.AtomicOrder.Monotonic);
     switch (st) {
         .inited => return,
         .deinited => return Error.double_init,
@@ -26,14 +30,14 @@ pub fn init() !void {
     }
 }
 pub fn deinit() !void {
-    var st = @atomicRmw(InitState, &init_st, std.builtin.AtomicRmwOp.Xchg, .deinited, std.builtin.AtomicOrder.Monotonic);
+    var st = @atomicRmw(InitState, &global.init_st, std.builtin.AtomicRmwOp.Xchg, .deinited, std.builtin.AtomicOrder.Monotonic);
     switch (st) {
         .inited => {},
         .deinited => return,
         .uninited => return Error.deinit_before_init,
     }
 
-    if (gpa.deinit() == .leak) return Error.gpa_leak;
+    if (heap.gpa.deinit() == .leak) return Error.gpa_leak;
 
     return;
 }
@@ -41,9 +45,9 @@ pub fn deinit() !void {
 test "module" {
     testing.log_level = std.log.Level.info;
     try init();
-    defer deinit() catch |err| std.debug.panic("error during deinit - {s}\n", .{@errorName(err)});
+    defer deinit() catch |err| std.debug.panic("error during deinit - {s}", .{@errorName(err)});
 
-    var str = try ally.dupe(u8, "test string");
-    defer ally.free(str);
+    var str = try heap.ally.dupe(u8, "test string");
+    defer heap.ally.free(str);
     // std.log.info("{s}\n", .{str});
 }
